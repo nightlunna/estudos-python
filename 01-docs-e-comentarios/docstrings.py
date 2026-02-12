47936a38fabcf51a253fc93f475fe927db2cd992
nightlunna <isabellyoliveirasp2018@gmail.com>
2026-02-12 11:28:13 -0300

    finalizei a parte de docstring realizando o mesmo de forma simples e organizada

---
+++ 01-docs-e-comentarios/docstrings.py
@@ -0,0 +1,5 @@
+""" Basicamente uma docstring é documentação de um código, ela é usada com três aspas, um exemplo
+    simples, seria quando você realizou um código a muito tempo e quer descobrir qual seria a
+    funcionalidade dele, para que ele serve em geral, na docstring você documenta o que cada
+    definição está dizendo ou fazendo, ou seja documentar.
+"""
