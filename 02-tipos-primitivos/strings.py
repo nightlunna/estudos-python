26ce478c86f8caf16798747e4f9f0b63f62b943e
nightlunna <isabellyoliveirasp2018@gmail.com>
2026-02-12 14:22:46 -0300

    finalizei a parte da string, onde codei e mostrei exemplos

---
+++ 02-tipos-primitivos/strings.py
@@ -0,0 +1,12 @@
+#strings são resumidamente cadeias de texto, onde podem ser manipuladas
+# cada uma delas são um campo a ser preenchido na memoria, é há vários tipos de variação no qual elas podem ser "fatiadas"
+
+# exemplo:
+# A p r e n d e n d o    P  y  t  h  o  n
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+
+# dito isso podemos manipular elas de diversas formas, exemplo:
+frase = "Aprendendo Python"
+frase[9::3]
+""" Ela vai estar pegando a string do número 9 pra frente mas pulando 3 letras até o final da string"""
+# existem outros claro, meio de maniuplar essa string, porém esse é um deles.
