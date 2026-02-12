135c45739358daf00bb2a921b14192f1a05926c2
nightlunna <isabellyoliveirasp2018@gmail.com>
2026-02-12 16:06:27 -0300

    adicionei um exemplo e finalizei a parte de inteiros

---
+++ 02-tipos-primitivos/inteiros.py
@@ -0,0 +1,9 @@
+"basicamente são números inteiros ou negativos sem casa decimal (100, 90, -100, etc)"
+"ele são representados pelo tipo INT "
+"temos alguns códigos que eles podem ser utilizados como a soma e subtração, listas, etc"
+#por exemplo
+idade = 26
+quantidade = 10
+saldo = 600
+
+print (quantidade, idade, saldo)
