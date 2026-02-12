21fa448a17097a56fb2c9102bfa396634b229a24
nightlunna <isabellyoliveirasp2018@gmail.com>
2026-02-12 11:21:04 -0300

    finalizei a parte de comentários realizando exemplos simples

---
+++ 01-docs-e-comentarios/comentarios.py
@@ -0,0 +1,5 @@
+# Estudo de comentários
+
+# Basicamante, qualquer coisa escrita dentro da "#", será ignorada no código.
+# Ela serve únicamente para comentar algo no código, ela não se lê.
+# Tudo que for comentado, deverá obrigatoriamente ser iniciado com "#".
